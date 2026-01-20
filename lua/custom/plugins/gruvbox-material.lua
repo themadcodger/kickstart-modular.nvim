4aff261d799225f27ad1a9dd1bd57ff5255d42ae
themadcodger <green.base8525@fastmail.com>
2026-01-20 14:55:00 -0800

    Fix configs for gruvbox-material and vimwiki

--- lua/custom/plugins/gruvbox-material.lua
+++ lua/custom/plugins/gruvbox-material.lua
@@ -6,11 +6,8 @@ return {
     config = function()
       -- Optionally configure and load the colorscheme
       -- directly inside the plugin declaration.
-<<<<<<< HEAD
       -- vim.g.gruvbox_material_enable_italic = false
-=======
       vim.g.gruvbox_material_enable_italic = true
->>>>>>> 9bc0baf (Add gruvbox-material theme and deprioritize tokyonight. Enable linebreak in options.lua)
       vim.g.gruvbox_material_background = 'soft'
       vim.g.gruvbox_material_transparent_background = 1
       vim.cmd.colorscheme 'gruvbox-material'
--- lua/custom/plugins/vimwiki.lua
+++ lua/custom/plugins/vimwiki.lua
@@ -10,11 +10,7 @@ return {
     vim.g.vimwiki_list = {
       {
         -- Here will be the path for your wiki
-<<<<<<< HEAD
         path = '~/OpenCloud/Personal/Documents/Personal/vimwiki',
-=======
-        path = "~/Sync'd/Documents/Personal/vimwiki/",
->>>>>>> b2c19f9 (Add vimwiki lua file)
         -- The syntax for the wiki
         syntax = 'markdown',
         ext = 'md',
